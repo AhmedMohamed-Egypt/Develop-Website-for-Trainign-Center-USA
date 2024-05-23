88c64f1aedf5db35db62673d2078b29e038c98b5
Ahmed-Mohamed-Egypt <ahmedatyabas@gmail.com>
2024-05-23 03:17:07 +0300

    add-hero-area

--- project/js/file.js
+++ project/js/file.js
@@ -1,1 +1,9 @@
-console.log(4)
+function translateElemnts(){
+    const allImgs = document.querySelectorAll(".herocontent__leftside--managers--imgs img")
+    allImgs.forEach((item,index)=>{
+        if(index !==0 ) {
+            item.style.transform=(`translateX(${-10*(index)}px)`)
+        }
+    })
+}
+translateElemnts()
