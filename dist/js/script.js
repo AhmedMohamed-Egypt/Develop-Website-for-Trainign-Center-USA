af3c771ac61ab7cec713b886629cf76933f1f3b3
Ahmed-Mohamed-Egypt <ahmedatyabas@gmail.com>
2024-05-24 18:50:15 +0300

    add-fixed-nav

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,36 +1,47 @@
-function translateElemnts(){
-    const allImgs = document.querySelectorAll(".herocontent__leftside--managers--imgs img")
-    allImgs.forEach((item,index)=>{
-        if(index !==0 ) {
-            item.style.transform=(`translateX(${-10*(index)}px)`)
-        }
-    })
+function translateElemnts() {
+  const allImgs = document.querySelectorAll(
+    ".herocontent__leftside--managers--imgs img"
+  );
+  allImgs.forEach((item, index) => {
+    if (index !== 0) {
+      item.style.transform = `translateX(${-10 * index}px)`;
+    }
+  });
 }
-translateElemnts()
+translateElemnts();
 
 const swiper = new Swiper(".testmonial__swiper .swiper", {
-    // Default parameters
-    slidesPerView: 3,
-    spaceBetween: 5,
-    speed: 500,
+  // Default parameters
+  slidesPerView: 3,
+  spaceBetween: 5,
+  speed: 500,
+
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 320px
+    320: {},
+    // when window width is >= 480px
+    480: {},
+    // when window width is >= 640px
+    640: {},
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+//scrollnavbar
+
+function scrollingNavBar(scrolling) {
   
-    // Responsive breakpoints
-    breakpoints: {
-      // when window width is >= 320px
-      320: {
-       
-      },
-      // when window width is >= 480px
-      480: {
-       
-      },
-      // when window width is >= 640px
-      640: {
-        
-      },
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
+
+  if (scrolling > 50) {
+    document.body.classList.add("fixed");
+  } else {
+    document.body.classList.remove("fixed");
+  }
+}
+window.onscroll =()=>{
+  scrollingNavBar(window.scrollY)
+}
