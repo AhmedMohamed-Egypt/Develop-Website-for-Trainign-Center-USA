829fab1f10efd7b5d0cfae35ca28115960ee5da9
Ahmed-Mohamed-Egypt <ahmedatyabas@gmail.com>
2024-06-26 15:40:40 +0300

    update

--- project/js/file.js
+++ project/js/file.js
@@ -1,4 +1,3 @@
-
 let plan;
 let savingResult;
 let error;
@@ -14,13 +13,15 @@ const calcButton = document.querySelector(
   ".saveTime__estimate--formTwo button"
 );
 const signUpBtn = document.querySelectorAll(".signBtn");
-const allChoosePlan = document.querySelectorAll(".choosePlan")
+const allChoosePlan = document.querySelectorAll(".choosePlan");
 const closeBtnModal = document.querySelector(".modalSignUp__closeBtn");
-const closeBtnModalRegister = document.querySelector(".modalRegister__closeBtn");
-const backButtonRegister = document.querySelector(".modalRegister__backBtn")
-const registerForm = document.querySelector(".registerForm")
-const yearlyPrices = document.querySelectorAll(".cardpricing__desc")
-let stepsRegister = 0
+const closeBtnModalRegister = document.querySelector(
+  ".modalRegister__closeBtn"
+);
+const backButtonRegister = document.querySelector(".modalRegister__backBtn");
+const registerForm = document.querySelector(".registerForm");
+const yearlyPrices = document.querySelectorAll(".cardpricing__desc");
+let stepsRegister = 0;
 function translateElemnts() {
   const allImgs = document.querySelectorAll(
     ".herocontent__leftside--managers--imgs img"
@@ -148,6 +149,9 @@ activateClassNavBar();
 
 function swiperPricingPage() {
   const swiperPricing = document.querySelector(".pricingPage__swiper .swiper");
+  const swiperRegister = document.querySelector(
+    ".registerCompany__cards .swiper"
+  );
   if (swiperPricing) {
     const swiper = new Swiper(".pricingPage__swiper .swiper", {
       // Default parameters
@@ -174,8 +178,40 @@ function swiperPricingPage() {
         },
       },
       navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+        nextEl: ".pricingPage__swiper .swiper-button-next",
+        prevEl: ".pricingPage__swiper .swiper-button-prev",
+      },
+    });
+  }
+  if (swiperCards) {
+    const swiper = new Swiper(".registerCompany__cards .swiper", {
+      // Default parameters
+      slidesPerView: 3,
+      spaceBetween: 5,
+      speed: 500,
+
+      // Responsive breakpoints
+      breakpoints: {
+        // when window width is >= 320px
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 25,
+        },
+        // when window width is >= 480px
+        767: {
+          slidesPerView: 2,
+        },
+        // when window width is >= 640px
+        1200: {
+          slidesPerView: 3,
+        },
+        1500: {
+          slidesPerView: 1,
+        },
+      },
+      navigation: {
+        nextEl: ".registerCompany__cards .swiper-button-next",
+        prevEl: ".registerCompany__cards .swiper-button-prev",
       },
     });
   }
@@ -183,47 +219,50 @@ function swiperPricingPage() {
 swiperPricingPage();
 
 //toggle Button
-
+const fixedPrice = [30, 60, 130, 220];
+const toggleBtnsPricing = document.querySelectorAll(
+  ".pricingPage__container--toggleBtns button"
+);
+const allPrices = document.querySelectorAll(".cardpricing__price--value");
+const allPricesRegister = document.querySelectorAll(
+  ".registerCompany__cards .cardpricing__price--value"
+);
 function toggleBtns() {
-  const fixedPrice = [30, 60, 130, 220];
-  const toggleBtns = document.querySelectorAll(
-    ".pricingPage__container--toggleBtns button"
-  );
-  const subscription = document.querySelector(
-    ".pricingPage__container--subscription"
-  );
-  const allPrices = document.querySelectorAll(".cardpricing__price--value");
-  function fillPrices(prices) {
-    allPrices.forEach((item, index) => {
+  function fillPrices(prices, priceElmnts) {
+    priceElmnts.forEach((item, index) => {
       item.textContent = prices[index];
     });
   }
-  fillPrices(fixedPrice);
+  fillPrices(fixedPrice, allPrices);
+  fillPrices(fixedPrice, allPricesRegister);
 
-  toggleBtns.forEach((item, index) => {
+  toggleBtnsPricing.forEach((item, index) => {
     if (item) {
       item.addEventListener("click", () => {
-        for (let i = 0; i < toggleBtns.length; i++) {
-          toggleBtns[i].classList.remove("active");
+        for (let i = 0; i < toggleBtnsPricing.length; i++) {
+          toggleBtnsPricing[i].classList.remove("active");
         }
         item.classList.add("active");
-        
+
         if (index === 1) {
-        //  subscription.classList.add("animate__animated", "animate__bounce");
-        yearlyPrices.forEach((item)=>{
-          item.textContent = "Monthly price based on annual subscription"
-        })
+          //  subscription.classList.add("animate__animated", "animate__bounce");
+          yearlyPrices.forEach((item) => {
+            item.textContent = "Monthly price based on annual subscription";
+          });
         } else {
-          yearlyPrices.forEach((item)=>{
-            item.textContent = " Monthly price based on 6 month subscription"
-          })
+          yearlyPrices.forEach((item) => {
+            item.textContent = " Monthly price based on 6 month subscription";
+          });
         }
-        
+
         if (index === 1) {
           const applyDiscount = fixedPrice.map((item) => item - item * 0.2);
-          fillPrices(applyDiscount);
+          fillPrices(applyDiscount, allPrices);
+          fillPrices(fixedPrice, allPricesRegister);
         } else {
-          fillPrices(fixedPrice);
+          fillPrices(fixedPrice, allPrices);
+
+          fillPrices(fixedPrice, allPricesRegister);
         }
       });
     }
@@ -240,21 +279,23 @@ function addClassPage() {
 
   if (arrayClasses.toString().indexOf("pricingPage") > -1) {
     document.body.classList.add("pricing");
-  } else  {
+  } else {
     document.body.classList.remove("pricing");
   }
-  if(arrayClasses.toString().indexOf('aboutPage')>-1){
-    const txtAbout = document.querySelector('.aboutPage__sectionOne--leftside--txt')
-   // txtAbout.style.maxHeight= window.innerHeight + "px"
-   const wHeight = window.innerHeight
-   const txtHeight = txtAbout.clientHeight
-   const protionHeight = (wHeight*50) / 100
-   txtAbout.style.height = (wHeight*90) / 100 + "px"
-   if(txtHeight > protionHeight) {
-    document.body.classList.add('adjutment')
-   }else {
-    document.body.classList.remove('adjutment')
-   }
+  if (arrayClasses.toString().indexOf("aboutPage") > -1) {
+    const txtAbout = document.querySelector(
+      ".aboutPage__sectionOne--leftside--txt"
+    );
+    // txtAbout.style.maxHeight= window.innerHeight + "px"
+    const wHeight = window.innerHeight;
+    const txtHeight = txtAbout.clientHeight;
+    const protionHeight = (wHeight * 50) / 100;
+    txtAbout.style.height = (wHeight * 90) / 100 + "px";
+    if (txtHeight > protionHeight) {
+      document.body.classList.add("adjutment");
+    } else {
+      document.body.classList.remove("adjutment");
+    }
   }
   //document.body.children.map((item)=>console.log(item))
 }
@@ -290,14 +331,14 @@ if (
     $(".saveTime__estimate--formTwo--inputGroup--input.select").niceSelect();
   });
 }
-//scrolling to calculator 
+//scrolling to calculator
 
-function scrollingCalculator(){
-  const saveTimeSection = document.querySelector(".saveTime")
-   
+function scrollingCalculator() {
+  const saveTimeSection = document.querySelector(".saveTime");
+
   window.scroll({
     top: saveTimeSection.offsetTop,
-    
+
     behavior: "smooth",
   });
 }
@@ -329,7 +370,7 @@ function calculateSaving() {
   };
 
   const keysData = Object.keys(data);
-  
+
   function fillingError(erorrTxt) {
     errorForm.textContent = erorrTxt;
   }
@@ -337,12 +378,11 @@ function calculateSaving() {
     savingResulInput.classList.add("active");
   }
 
-
   allInputs.forEach((item, index) => {
     item.addEventListener("keyup", () => {
       for (let i = 0; i < allInputs.length; i++) {
         if (index === i) {
-          if (!isNaN(item.value) && item.value.trim().length !== 0 ) {
+          if (!isNaN(item.value) && item.value.trim().length !== 0) {
             data[keysData[i]] = +item.value;
             allParent[index].classList.add("right");
             allParent[index].classList.remove("wrong");
@@ -353,8 +393,10 @@ function calculateSaving() {
           }
         }
       }
-      if (data.DewdroppersTraining > 0 &&   data.livingTraing <= data.DewdroppersTraining) {
-       
+      if (
+        data.DewdroppersTraining > 0 &&
+        data.livingTraing <= data.DewdroppersTraining
+      ) {
         fillingError(
           "Live Training should be greater than Dewdroppers Training"
         );
@@ -377,14 +419,14 @@ function calculateSaving() {
       if (error) {
         calcButton.classList.remove("active");
         calcButton.setAttribute("disabled", true);
-        savingResulInput.value = "00000"
+        savingResulInput.value = "00000";
       } else {
         calcButton.classList.add("active");
         calcButton.removeAttribute("disabled");
       }
     });
   });
- 
+
   if (calcButton) {
     calcButton.addEventListener("click", () => {
       const slotOne =
@@ -397,128 +439,158 @@ function calculateSaving() {
         fillingError("Please Review equivalent Plan");
       } else {
         fillingError("");
-        const resultCalc = (data.livingTraing - data.DewdroppersTraining) *
-        data.staffRate *
-        data.noOfStaff *
-        data.noOfCamapign - plan
-        savingResult = resultCalc.toLocaleString('en-US');
-     //   activeSavingResult();
-       // savingResulInput.setAttribute("placeholder", savingResult);
-       savingResulInput.value = savingResult
+        const resultCalc =
+          (data.livingTraing - data.DewdroppersTraining) *
+            data.staffRate *
+            data.noOfStaff *
+            data.noOfCamapign -
+          plan;
+        savingResult = resultCalc.toLocaleString("en-US");
+        //   activeSavingResult();
+        // savingResulInput.setAttribute("placeholder", savingResult);
+        savingResulInput.value = savingResult;
       }
-      scrollingCalculator()
+      scrollingCalculator();
     });
   }
 }
 calculateSaving();
 //distibute popup
 
-function distributePop(){
-  const allPops = document.querySelectorAll(".saveTime__estimate--formTwo--inputGroup--popup")
-  allPops.forEach((item)=>{
-    if(item){
-      console.log(item.clientHeight + 20 )
-      item.style.top = -(item.clientHeight+10)+"px"
+function distributePop() {
+  const allPops = document.querySelectorAll(
+    ".saveTime__estimate--formTwo--inputGroup--popup"
+  );
+  allPops.forEach((item) => {
+    if (item) {
+      console.log(item.clientHeight + 20);
+      item.style.top = -(item.clientHeight + 10) + "px";
     }
-  })
+  });
 }
-distributePop()
+distributePop();
 
-//Register Form 
+//Register Form
 
-let activePersonal=false;
-let activeCompany = false
-const radioInput = document.querySelectorAll(".registerForm__selectors input")
-const personalWidget = document.querySelector('.registerForm__personal')
-const companyWidget = document.querySelector('.registerForm__company')
-const selectorsIdentity = document.querySelector(".registerForm__selectors")
+let activePersonal = false;
+let activeCompany = false;
+let step = 0;
+let stepSpan = -1;
+let selectCompany = true;
+const radioInput = document.querySelectorAll(".registerForm__selectors input");
+const personalWidget = document.querySelector(".registerForm__personal");
+const companyWidget = document.querySelector(".registerForm__company");
+const selectorsIdentity = document.querySelector(".registerForm__selectors");
 
-function hideSelectors(){
-  selectorsIdentity.classList.add('hide')
-}
-function showActiveIdintity(persoanlParam,companyParam){
- if(persoanlParam){
-  personalWidget.classList.add('active')
-  companyWidget.classList.remove('active')
- 
- } 
-  if(companyParam) {
-    companyWidget.classList.add('active')
-    personalWidget.classList.remove('active')
+const nextBtn = document.querySelector(
+  ".registerForm__company__container--next"
+);
+const allComapnies = document.querySelectorAll(".companysteps");
+const allSpanSteps = document.querySelectorAll(
+  ".registerForm__company__container--steps span"
+);
+function activeSlide() {
+  allComapnies.forEach((item, index) => {
+
+    if (index === step) {
+    item.classList.add("active")
+    }else {
+      item.classList.remove("active")
+    }
     
+  });
+}
+function removeActiveSlide(){
+  allComapnies.forEach((item, index) => {
+  
+    if (index === step) {
+    item.classList.remove("active")
+    }
+    
+  });
+}
+function progressSpan() {
+  allSpanSteps.forEach((item, index) => {
+    if (index === stepSpan) {
+      item.classList.add("active");
+    }
+  });
+}
+function hideSelectors() {
+  selectorsIdentity.classList.add("hide");
+}
+function showActiveIdintity(persoanlParam, companyParam) {
+  if (persoanlParam) {
+    personalWidget.classList.add("active");
+    companyWidget.classList.remove("active");
+  }
+  if (companyParam) {
+    companyWidget.classList.add("active");
+    personalWidget.classList.remove("active");
   }
 }
-function togglePersonalCompany(){
-   radioInput.forEach((item)=>{
-    item.addEventListener('click',()=>{
-      registerForm.classList.add('active')
-      hideSelectors()
-      stepsRegister++
-      if(item.getAttribute('id')=='personal'){
-        activePersonal = true
-        activeCompany = false
-      }else {
-        activeCompany = true
-        activePersonal = true
+function togglePersonalCompany() {
+  radioInput.forEach((item) => {
+    item.addEventListener("click", () => {
+      registerForm.classList.add("active");
+      hideSelectors();
+      stepsRegister++;
+      if (item.getAttribute("id") == "personal") {
+        activePersonal = true;
+        activeCompany = false;
+        selectCompany = false;
+      } else {
+        activeCompany = true;
+        activePersonal = true;
+        selectCompany = true;
       }
-      showActiveIdintity(activePersonal,activeCompany)
-       backButton()
-    })
-   })
-    
-  
+      showActiveIdintity(activePersonal, activeCompany);
+      backButton();
+    });
+  });
 }
-togglePersonalCompany()
+togglePersonalCompany();
 
-
-
-function backButton(){
-  
-  if(stepsRegister>0){
-    backButtonRegister.classList.add('active')
-  }else {
-    backButtonRegister.classList.remove('active')
-   
-
+function backButton() {
+  if (stepsRegister > 0) {
+    backButtonRegister.classList.add("active");
+  } else {
+    backButtonRegister.classList.remove("active");
   }
-  backButtonRegister.addEventListener('click',()=>{
-   personalWidget.classList.remove('active')
-   companyWidget.classList.remove('active')
-   selectorsIdentity.classList.remove('hide')
-   backButtonRegister.classList.remove('active')
-   registerForm.classList.remove('active')
-   activeCompany=false
-   activePersonal=false
-    
-  })
-}
-function compnayFlow(){
-  const nextBtn = document.querySelector(".registerForm__company__container--next")
-  const allComapnies = document.querySelectorAll(".companysteps")
-  let step = 0
-  function activeSlide(){
-    allComapnies.forEach((item,index)=>{
-  
-      if(index===step){
-        item.classList.add('active')
-      }else {
-        item.classList.remove('active')
+
+  backButtonRegister.addEventListener("click", () => {
+    if (!selectCompany) {
+      personalWidget.classList.remove("active");
+      companyWidget.classList.remove("active");
+      selectorsIdentity.classList.remove("hide");
+      backButtonRegister.classList.remove("active");
+      registerForm.classList.remove("active");
+      activeCompany = false;
+      activePersonal = false;
+    } else {
+      if (stepSpan >= 0 || step>=1) {
+        stepSpan--;
+        step--
       }
-     })
-  }
-  activeSlide()
-  
- if(nextBtn){
-  nextBtn.addEventListener('click',(e)=>{
-    e.preventDefault();
-    step++
-    activeSlide()
-  })
- 
- }
+      
 
- console.log(step)
- 
-
+      removeActiveSlide()
+      progressSpan();
+    }
+  });
 }
-compnayFlow()
+function compnayFlow() {
+  //activeSlide();
+
+  if (nextBtn) {
+    nextBtn.addEventListener("click", (e) => {
+      e.preventDefault();
+      step++;
+      stepSpan++;
+      activeSlide();
+      progressSpan();
+      
+    });
+  }
+}
+compnayFlow();
